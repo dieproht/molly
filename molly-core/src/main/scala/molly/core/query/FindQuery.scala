3614ad2aa85a3ee4f34c2d777275b5541fd550fe
Karl F Walkow <38581665+dieproht@users.noreply.github.com>
2023-09-26 16:47:19 +0200

    Find first element in results (#6)

--- molly-core/src/main/scala/molly/core/query/FindQuery.scala
+++ molly-core/src/main/scala/molly/core/query/FindQuery.scala
@@ -8,6 +8,11 @@ import org.bson.BsonDocument
 
 final case class FindQuery[F[_]: Async] private[core] (private[core] val publisher: FindPublisher[BsonDocument]) {
 
+   def first: F[Option[BsonDocument]] =
+      limit(1).stream.head.compile.fold(Option.empty[BsonDocument])((_, doc) => Some(doc))
+
+   def limit(limit: Int): FindQuery[F] = FindQuery(publisher.limit(limit))
+
    def list: F[List[BsonDocument]] = stream.compile.toList
 
    def stream: Stream[F, BsonDocument] = fromPublisher(publisher, bufferSize = 1)
