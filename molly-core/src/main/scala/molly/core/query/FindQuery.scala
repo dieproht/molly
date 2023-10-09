786c7b840b87a0c13644037ceef723a77b47212c
Karl F Walkow <karl@walkow.de>
2023-10-09 15:52:46 +0200

    Find with chained filter

--- molly-core/src/main/scala/molly/core/query/FindQuery.scala
+++ molly-core/src/main/scala/molly/core/query/FindQuery.scala
@@ -5,13 +5,16 @@ import com.mongodb.reactivestreams.client.FindPublisher
 import fs2.Stream
 import molly.core.reactivestreams.fromPublisher
 import org.bson.BsonDocument
+import org.bson.conversions.Bson
 
 final case class FindQuery[F[_]: Async] private[core] (private[core] val publisher: FindPublisher[BsonDocument]) {
 
-   def first: F[Option[BsonDocument]] =
-      limit(1).stream.head.compile.fold(Option.empty[BsonDocument])((_, doc) => Some(doc))
+   def filter(filter: Bson) = FindQuery(publisher.filter(filter))
 
    def limit(limit: Int): FindQuery[F] = FindQuery(publisher.limit(limit))
+
+   def first: F[Option[BsonDocument]] =
+      limit(1).stream.head.compile.fold(Option.empty[BsonDocument])((_, doc) => Some(doc))
 
    def list: F[List[BsonDocument]] = stream.compile.toList
 
