1a85b3b5d03ad70598bd8f32ab0370c472582563
Karl F Walkow <karl@walkow.de>
2023-11-24 10:24:19 +0100

    Support fullDocument in watch

--- molly-core/src/main/scala/molly/core/query/WatchQuery.scala
+++ molly-core/src/main/scala/molly/core/query/WatchQuery.scala
@@ -2,6 +2,7 @@ package molly.core.query
 
 import cats.effect.kernel.Async
 import com.mongodb.client.model.changestream.ChangeStreamDocument
+import com.mongodb.client.model.changestream.FullDocument
 import com.mongodb.reactivestreams.client.ChangeStreamPublisher
 import fs2.Stream
 import molly.core.reactivestreams.fromStreamPublisher
@@ -18,6 +19,10 @@ final case class WatchQuery[F[_]: Async] private[core] (
    /** [[https://mongodb.github.io/mongo-java-driver/4.10/apidocs/mongodb-driver-reactivestreams/com/mongodb/reactivestreams/client/ChangeStreamPublisher.html#batchSize(int)]]
      */
    def batchSize(batchSize: Int): WatchQuery[F] = WatchQuery(publisher.batchSize(batchSize))
+
+   /** [[https://mongodb.github.io/mongo-java-driver/4.10/apidocs/mongodb-driver-reactivestreams/com/mongodb/reactivestreams/client/ChangeStreamPublisher.html#fullDocument(com.mongodb.client.model.changestream.FullDocument)]]
+     */
+   def fullDocument(fullDocument: FullDocument): WatchQuery[F] = WatchQuery(publisher.fullDocument(fullDocument))
 
    def list: F[List[ChangeStreamDocument[BsonDocument]]] = stream.compile.toList
 
