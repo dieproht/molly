e0b3f1c5c3ec09e7a6bf00969b86a93fc999684d
Karl F Walkow <38581665+dieproht@users.noreply.github.com>
2024-06-04 15:12:12 +0200

    Migrate to Scala 3 extension syntax (#98)

--- molly-core/src/main/scala/molly/core/syntax/filters.scala
+++ molly-core/src/main/scala/molly/core/syntax/filters.scala
@@ -3,19 +3,15 @@ package molly.core.syntax
 import com.mongodb.client.model.Filters
 import org.bson.conversions.Bson
 
-import scala.language.implicitConversions
-
 trait filters:
-  implicit def mollyFiltersSyntax(filter: Bson): FiltersExtensions =
-    new FiltersExtensions(filter)
+
+  extension (filter: Bson)
+    def and(anotherFilter: Bson): Bson = Filters.and(filter, anotherFilter)
+
+    def or(anotherFilter: Bson): Bson = Filters.or(filter, anotherFilter)
+
+    def not: Bson = Filters.not(filter)
+
+    def nor: Bson = Filters.nor(filter)
 
 object filters extends filters
-
-final class FiltersExtensions(filter: Bson):
-  def and(anotherFilter: Bson): Bson = Filters.and(filter, anotherFilter)
-
-  def or(anotherFilter: Bson): Bson = Filters.or(filter, anotherFilter)
-
-  def not: Bson = Filters.not(filter)
-
-  def nor: Bson = Filters.nor(filter)
