60c96c7a74000737b83843bdceb87a360526f92a
Karl F Walkow <karl@walkow.de>
2024-06-04 09:00:56 +0200

    Update links to MongoDB docs

--- molly-core/src/main/scala/molly/core/MollyClient.scala
+++ molly-core/src/main/scala/molly/core/MollyClient.scala
@@ -9,11 +9,11 @@ import com.mongodb.reactivestreams.client.MongoClient
 import com.mongodb.reactivestreams.client.MongoClients
 
 /** Molly's counterpart to
-  * [[https://mongodb.github.io/mongo-java-driver/4.11/apidocs/mongodb-driver-reactivestreams/com/mongodb/reactivestreams/client/MongoClient.html MongoClient]].
+  * [[https://mongodb.github.io/mongo-java-driver/5.1/apidocs/mongodb-driver-reactivestreams/com/mongodb/reactivestreams/client/MongoClient.html MongoClient]].
   */
 final case class MollyClient[F[_]: Async] private (private[core] val delegate: MongoClient) {
 
-   /** [[https://mongodb.github.io/mongo-java-driver/4.11/apidocs/mongodb-driver-reactivestreams/com/mongodb/reactivestreams/client/MongoClient.html#getDatabase(java.lang.String)]]
+   /** [[https://mongodb.github.io/mongo-java-driver/5.1/apidocs/mongodb-driver-reactivestreams/com/mongodb/reactivestreams/client/MongoClient.html#getDatabase(java.lang.String)]]
      */
    def getDatabase(name: String): F[MollyDatabase[F]] =
       Async[F].delay(delegate.getDatabase(name)).map(MollyDatabase(_))
@@ -24,7 +24,7 @@ final case class MollyClient[F[_]: Async] private (private[core] val delegate: M
    def getDatabaseAsResource(name: String): Resource[F, MollyDatabase[F]] =
       Resource.eval(getDatabase(name))
 
-   /** [[https://mongodb.github.io/mongo-java-driver/4.11/apidocs/mongodb-driver-reactivestreams/com/mongodb/reactivestreams/client/MongoClient.html#getClusterDescription()]]
+   /** [[https://mongodb.github.io/mongo-java-driver/5.1/apidocs/mongodb-driver-reactivestreams/com/mongodb/reactivestreams/client/MongoClient.html#getClusterDescription()]]
      */
    def getClusterDescription(): ClusterDescription = delegate.getClusterDescription()
 }
