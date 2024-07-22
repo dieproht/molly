d36e7ffac132984d9d22722aa073041b24b47220
Karl F Walkow <karl@walkow.de>
2024-07-22 14:53:23 +0200

    Watch collection with aggregation pipeline

--- molly-core/src/main/scala/molly/core/MollySyncCollection.scala
+++ molly-core/src/main/scala/molly/core/MollySyncCollection.scala
@@ -4,6 +4,9 @@ import cats.effect.kernel.Async
 import com.mongodb.client.MongoCollection
 import molly.core.query.SyncWatchQuery
 import org.bson.BsonDocument
+import org.bson.conversions.Bson
+
+import scala.jdk.CollectionConverters.*
 
 /** Molly's counterpart to
   * [[https://mongodb.github.io/mongo-java-driver/5.1/apidocs/mongodb-driver-sync/com/mongodb/client/MongoCollection.html MongoCollection]].
@@ -17,3 +20,8 @@ final case class MollySyncCollection[F[_], A] private[core] (private[core] val d
   /** [[https://mongodb.github.io/mongo-java-driver/5.1/apidocs/mongodb-driver-sync/com/mongodb/client/MongoCollection.html#watch()]]
     */
   def watch(): SyncWatchQuery[F, A] = SyncWatchQuery(delegate.watch(classOf[BsonDocument]))
+
+  /** [[https://mongodb.github.io/mongo-java-driver/5.1/apidocs/mongodb-driver-sync/com/mongodb/client/MongoCollection.html#watch(java.util.List)]]
+    */
+  def watch(pipeline: Seq[Bson]): SyncWatchQuery[F, A] =
+    SyncWatchQuery(delegate.watch(pipeline.asJava, classOf[BsonDocument]))
